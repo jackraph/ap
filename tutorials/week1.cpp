3e297f3533ddfae3c0b7fb075f78ba5dabf409da
Jackson <s3884794@student.rmit.edu.au>
2022-06-05 10:01:11 +1000

    Turorial Week 1 - Progress

--- tutorials/week1.cpp
+++ tutorials/week1.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <string>
 int main ( void ) {
-  std :: cout << " Hello World !" << std :: endl ;
+
+  //Question 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // std::cout << "Hello World !" << std::endl;
+
+  //Question 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // std::cout <<"Enter your name:" << std::endl;
+  // std::string name = "";
+  // std::cin >> name;
+  //
+  // std::cout <<"Enter your age:" << std::endl;
+  // std::string age = "";
+  // std::cin >> age;
+  //
+  // std::cout <<"Enter your height:" << std::endl;
+  // std::string height = "";
+  // std::cin >> height;
+  //
+  // std::cout <<"Your name is: " + name << std::endl;
+  // std::cout <<"Your age is: " + age << std::endl;
+  // std::cout <<"Your height is: " + height << std::endl;
+
+  //Question 3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+
   return EXIT_SUCCESS ;
 }
