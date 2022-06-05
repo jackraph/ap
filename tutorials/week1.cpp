fe9d1471321e973cccc1f38caf47d67ffb69ee52
Jackson <s3884794@student.rmit.edu.au>
2022-06-05 10:45:56 +1000

    Tutorial 1 - Progress

--- tutorials/week1.cpp
+++ tutorials/week1.cpp
@@ -23,7 +23,38 @@ int main ( void ) {
   // std::cout <<"Your height is: " + height << std::endl;
 
   //Question 3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  
+  //a)
+  for(int i = 1; i <= 6; i++) {
+
+    for(int j = 1; j <= i; j++) {
+
+      std::cout << "*";
+    }
+
+    std::cout <<"\n";
+  }
+  std::cout <<"-------------------" << std::endl;
+
+  //b)
+  for(int i = 1; i <= 6; i++) {
+
+    for(int j = 6; j >= 1; j--) {
+
+      if(j <= i) {
+
+        std::cout << "*";
+      } else {
+
+        std::cout << " ";
+      }
+    }
+
+    std::cout <<"\n";
+  }
+  std::cout <<"-------------------" << std::endl;
+
+  //c)
+
 
   return EXIT_SUCCESS ;
 }
