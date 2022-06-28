9b36602e9cdd3697356201fc76a9bfaf19a5e76e
Jackson <s3884794@student.rmit.edu.au>
2022-06-28 20:52:29 +1000

    Working on pathsolver algo

--- a1/Node.cpp
+++ a1/Node.cpp
@@ -11,8 +11,8 @@ Node::Node(int row, int col, int dist_traveled)
 
 Node::~Node(){
     // TODO~~~~~~~~~~~~~
-    //DECONSTRUCTOR CODE HERE!!!
-    //TODO ~~~~~~~~~~
+    // DECONSTRUCTOR CODE HERE!!!
+    // TODO ~~~~~~~~~~
 }
 
 int Node::getRow(){
