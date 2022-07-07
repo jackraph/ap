7ceb11c77e38455074c051fc431ce8c39c4e59fa
jackraph <s3884794@student.rmit.edu.au>
2022-07-07 10:29:43 +1000

    Week 6 tutorial complete

--- tutorials/week6/LinkedList.cpp
+++ tutorials/week6/LinkedList.cpp
@@ -24,25 +24,39 @@ void LinkedList::clear() {
     }
     delete current;
     this->count = 0;
+    this->head = nullptr;
+    this->back = nullptr;
 }
 
 int LinkedList::get(int i) {
     Node* node = this->head;
-    for(int j = 1; j < i; j++) {
+    for(int j = 0; j < i; j++) {
         node = node->next;
     }
     return node->data;
 }
 
 void LinkedList::addFront(int data) {
-    Node* newNode = new Node(data, this->head);
+    Node* newNode = nullptr;
+    if(this->count < 1) { 
+        // This is the only node in chain, store it as back node & head node
+        newNode = new Node(data, nullptr);
+        this->back = newNode;
+    } else {
+        newNode = new Node(data, this->head);
+    }
     this->head = newNode;
     this->count++;
 }
 
 void LinkedList::addBack(int data) {
     Node* newNode = new Node(data, nullptr);
-    this->back->next = newNode;
+    if(this->count < 1) { 
+        // This is the only node in chain, store it as back node & head node
+        this->head = newNode;
+    } else {
+        this->back->next = newNode;
+    } 
     this->back = newNode;
     this->count++;
 }
--- tutorials/week6/main.cpp
+++ tutorials/week6/main.cpp
@@ -1,10 +1,72 @@
 #include <iostream>
-#include <string>
 #include <vector>
 
 #include "LinkedList.h"
 
 
-void main(int argc, char** argv){
-    
+int main(int argc, char** argv){
+
+    // QUESTION 1
+    //---------------------------------------------------------------------------------
+    // (a)Creates a C++ STL Vector of integers
+    std::vector<int> myVector;
+
+    // (b)Fills the vector with 100 integers from 0 to 99
+    for(int i = 0; i < 100; i++) {
+        myVector.push_back(i);
+    }
+
+    // (c)Prints out the contents of the vector as a comma separated list on a single line
+    for(int i = 0; i < (int)myVector.size(); i++) {
+        std::cout << myVector[i] << ",";
+    }
+    //---------------------------------------------------------------------------------
+
+
+    std::cout << std::endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+
+
+    // QUESTION 2
+    //---------------------------------------------------------------------------------
+    // (a)Write a code snippet to create a new Linked List
+    LinkedList* myList = new LinkedList();
+
+    // (b)Write additional code to add three integers to the Linked List
+    myList->addFront(5);
+    myList->addBack(6);
+    myList->addFront(4);
+
+    // (c)Write additional code to print out the contents of the Linked List in order
+    for(int i = 0; i < myList->size(); i++) {
+        std::cout << myList->get(i) << ",";
+    }
+
+    // (d)For each question above draw the picture of the Linked List after every step
+    /*
+        First Step Picture: [5]
+        Second Step Picture: [5]-[6]
+        Third Step Picture: [4]-[5]-[6]
+    */
+
+    delete myList;
+    //---------------------------------------------------------------------------------
+
+
+    std::cout << std::endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+
+
+    // QUESTION 3 - QUESTION 5. Repeat Q1 using linked list to demonstrate it working
+    //---------------------------------------------------------------------------------
+    LinkedList* myList = new LinkedList();
+    for(int i = 0; i < 100; i++) {
+        myList->addBack(i);
+    }
+    for(int i = 0; i < myList->size(); i++) {
+        std::cout << myList->get(i) << ",";
+    }
+    delete myList;
+    //---------------------------------------------------------------------------------
+
+
+    std::cout << std::endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
 }
